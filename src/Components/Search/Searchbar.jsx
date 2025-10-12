3c3d2447b5a96763db828e7f118a6685a137891d
sarbjeetmaan <sarbjeetkaurmaan650@gmail.com>
2025-10-12 07:59:13 -0400

    yes

--- src/Components/Search/Searchbar.jsx
+++ src/Components/Search/Searchbar.jsx
@@ -59,7 +59,7 @@ const SearchBar = () => {
           className="searchbar-input"
         />
         <button type="submit" className="searchbar-button">
-          🔍
+          🔍︎
         </button>
       </form>
 
